fb7c6cb885f308800d54f68da1fa9be5f4af2b7c
deft <deftmail@163.com>
2021-01-11 00:33:44 +0800

    camelize

--- src/test/util.test.ts
+++ src/test/util.test.ts
@@ -2,9 +2,9 @@ import { camelize } from '../util'
 
 test('camelize function test', () => {
   const foo1 = 'get-element-by-id'
-  const foo2 = 'Aaa-bbb-ccc'
+  const foo2 = 'aaa-bbb-ccc'
   const ret1 = camelize(foo1)
-  const ret2 = camelize(foo2)
+  const ret2 = camelize(foo2, true)
   expect(ret1).toBe('getElementById')
   expect(ret2).toBe('AaaBbbCcc')
 })
--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,11 @@
  * Camelize string
  */
 const camelizeRE = /-(\w)/g
-export const camelize = (str: string): string => {
+export const camelize = (str: string, big?: boolean): string => {
+  if (big) {return str.charAt(0).toUpperCase() + str.slice(1).replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '')}
   return str.replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '')
 }
+
+/**
+ * 
+ */

aa7a73bd67c08f48807f5142f797798ba073df84
deft <deftmail@163.com>
2021-01-27 01:26:36 +0800

    ✅ unique fn

--- src/test/util.test.ts
+++ src/test/util.test.ts
@@ -1,10 +1,28 @@
-import { camelize } from '../util'
+import { camelize, isPalindrome, reverseStr, unique } from '../util'
 
-test('camelize function test', () => {
-  const foo1 = 'get-element-by-id'
-  const foo2 = 'aaa-bbb-ccc'
-  const ret1 = camelize(foo1)
-  const ret2 = camelize(foo2, true)
-  expect(ret1).toBe('getElementById')
-  expect(ret2).toBe('AaaBbbCcc')
+describe('util test', () => {
+  test('camelize function', () => {
+    const foo1 = 'get-element-by-id'
+    const foo2 = 'aaa-bbb-ccc'
+    const ret1 = camelize(foo1)
+    const ret2 = camelize(foo2, true)
+    expect(ret1).toBe('getElementById')
+    expect(ret2).toBe('AaaBbbCcc')
+  })
+
+  test('isPalindrome function', () => {
+    const str = 'TENET'
+    expect(isPalindrome(str)).toBeTruthy()
+  })
+
+  test('reverseStr function', () => {
+    const str = 'hello world'
+    const str1 = reverseStr(str)
+    expect(str1).toBe('dlrow olleh')
+  })
+
+  test('unique function', () => {
+    const arr = [1, 2, 2, 3, 4, 5, 5, 6]
+    expect(unique(arr)).toEqual([1, 2, 3, 4, 5, 6])
+  })
 })
--- src/util.ts
+++ src/util.ts
@@ -14,10 +14,10 @@ export const camelize = (str: string, big?: boolean): string => {
  */
 export function cached<T extends (str: string) => any>(fn: T): T {
   const cache = Object.create(null)
-  return (function cached(str: string) {
+  return function cached(str: string) {
     const hit = cache[str]
     return hit || (cache[str] = fn(str))
-  }) as T
+  } as T
 }
 
 /**
@@ -36,8 +36,8 @@ export function compose(...funcs: Function[]) {
 /**
  * deepClone
  */
-export function deepClone (obj: any, hash = new WeakMap()){
-  if (obj === null || typeof obj !== 'object')  return obj
+export function deepClone(obj: any, hash = new WeakMap()) {
+  if (obj === null || typeof obj !== 'object') return obj
   if (obj instanceof Date) return new Date(obj)
   if (obj instanceof RegExp) return new RegExp(obj)
 
@@ -56,12 +56,12 @@ export function deepClone (obj: any, hash = new WeakMap()){
  * emitter
  */
 interface EmitterProps {
-  emit: (type: string) => void;
-  addListener: (type: string, fn: any) => void;
+  emit: (type: string) => void
+  addListener: (type: string, fn: any) => void
 }
 
-class Emitter implements EmitterProps {
-  events: any;
+export class Emitter implements EmitterProps {
+  events: any
   constructor() {
     this.events = this.events || new Map()
   }
@@ -79,8 +79,48 @@ class Emitter implements EmitterProps {
 }
 
 /**
- * throttle
+ * curry
  */
-function throttle() {
-  
+export function curry(fn: any) {
+  let allArgs: any = []
+  return function temp(...args: any) {
+    if (args.length === 0) {
+      return fn.apply(temp, allArgs)
+    } else {
+      ;[].push.apply(allArgs, args)
+      return temp
+    }
+  }
+}
+
+/**
+ * isPalindrome
+ */
+export function isPalindrome(s: string): boolean {
+  s = s.toLowerCase().replace(/[\W_]/g, '')
+  if (s.length < 2) return true
+  let left = 0
+  let right = s.length - 1
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false
+    }
+    left++
+    right--
+  }
+  return true
+}
+
+/**
+ * reverse string
+ */
+export function reverseStr(s: string) {
+  return s.split('').reverse().join('')
+}
+
+/**
+ * unique
+ */
+export function unique(arr: any[]) {
+  return [...new Set(arr)]
 }
